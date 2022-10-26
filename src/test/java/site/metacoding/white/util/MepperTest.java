d56c52eb3e3cae6504bff8e1c0c9ea0f485718a7
shyue02 <shyue02@naver.com>
2022-10-26 10:08:38 +0900

    MepperTest-고급매핑하기

--- src/test/java/site/metacoding/white/util/MepperTest.java
+++ src/test/java/site/metacoding/white/util/MepperTest.java
@@ -2,9 +2,14 @@ package site.metacoding.white.util;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+// 스프링에서 DB -> rs -> Entity (전략 : 디폴트 생성자를 호출한 뒤 setter)
+
+@NoArgsConstructor
 @Setter
 @Getter
 class Product {
@@ -13,6 +18,16 @@ class Product {
     private Integer price;
     private Integer qty;
     private String mcp; // 제조사
+
+    @Builder
+    public Product(Integer id, String name, Integer price, Integer qty, String mcp) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+        this.mcp = mcp;
+    }
+
 }
 
 @Setter // 데이터를 변경, history, 변화할 때 씀
@@ -21,35 +36,44 @@ class ProductDto {
     private String name;
     private Integer price;
     private Integer qty;
+
+    public ProductDto(Product product) {
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.qty = product.getQty();
+    }
+
+    public Product toEntity() {
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .qty(qty)
+                .build();
+    }
 }
 
 public class MepperTest {
 
     @Test
-    public void 매핑하기() {
-        // 1. Produdct 객체생성 (디폴트)
-        Product product = new Product();
-
-        // 2. 값 넣기
-        product.setId(1);
-        product.setName("바나나");
-        product.setPrice(1000);
-        product.setQty(100);
-        product.setMcp("삼성");
-
-        // 3. ProdudctDto 객체생성 (디폴트)
-        ProductDto productDto = new ProductDto();
-
-        // 4. Product -> ProductDto로 옮기기
-        productDto.setName(product.getName());
-        productDto.setPrice(product.getPrice());
-        productDto.setQty(product.getQty());
-
-        // 5. ProductDto -> product 변경
-        Product p2 = new Product();
-        p2.setName(productDto.getName());
-        p2.setPrice(productDto.getPrice());
-        p2.setQty(productDto.getQty());
+    public void 고급매핑하기() {
+        // toEntity, toDto 만들어서 매핑하면 편하지 않을까? (재활용)
     }
 
+    @Test
+    public void 매핑하기1() {
+        // 1. Product 객체생성 (디폴트)
+        Product product = Product.builder()
+                .id(1)
+                .mcp("삼성")
+                .name("바나나")
+                .price(1000)
+                .qty(100)
+                .build();
+
+        // 3. ProductDto 객체생성 (디폴트)
+        ProductDto productDto = new ProductDto(product);
+
+        // 5. ProductDto -> product 변경
+        Product product2 = productDto.toEntity();
+    }
 }
