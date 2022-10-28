cc790f6473b84e641b579979f919b30caea5658d
shyue02 <shyue02@naver.com>
2022-10-28 11:17:26 +0900

    UserApiController 테스트 완료

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -22,10 +22,12 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
 import site.metacoding.white.dto.UserReqDto.JoinReqDto;
+import site.metacoding.white.dto.UserReqDto.LoginReqDto;
+import site.metacoding.white.service.UserService;
 
 @ActiveProfiles("test")
 // @Transactional // 통합테스트에서 RANDOM_PORT를 사용하면 새로운 스레드로 돌기 때문에 rollback 무의미
-@Sql("classpath:truncate.sql")
+@Sql("classpath:truncate.sql") // 기본 정책 (전 - 후)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserApiControllerTest {
 
@@ -33,6 +35,8 @@ public class UserApiControllerTest {
     private TestRestTemplate rt;
     @Autowired
     private ObjectMapper om;
+    @Autowired
+    private UserService userService;
 
     private static HttpHeaders headers;
 
@@ -42,7 +46,7 @@ public class UserApiControllerTest {
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
-    @Order(1)
+    // @Order(1)
     @Test
     public void join_test() throws JsonProcessingException {
         // given
@@ -62,6 +66,8 @@ public class UserApiControllerTest {
         // System.out.println(response.getStatusCode());
         // System.out.println(response.getBody());
 
+        System.out.println("디버그 : " + response.getStatusCodeValue());
+
         DocumentContext dc = JsonPath.parse(response.getBody());
         // System.out.println(dc.jsonString());
         Integer code = dc.read("$.code");
@@ -70,28 +76,33 @@ public class UserApiControllerTest {
 
     @Order(2)
     @Test
-    public void join_test2() throws JsonProcessingException {
-        // given
+    public void login_test() throws JsonProcessingException {
+        // data init
         JoinReqDto joinReqDto = new JoinReqDto();
         joinReqDto.setUsername("very");
         joinReqDto.setPassword("1234");
+        userService.save(joinReqDto);
 
-        String body = om.writeValueAsString(joinReqDto);
-        System.out.println(body);
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("very");
+        loginReqDto.setPassword("1234");
+        String body = om.writeValueAsString(loginReqDto);
+        System.out.println("디버그 : " + body);
 
         // when
         HttpEntity<String> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+        ResponseEntity<String> response = rt.exchange("/login", HttpMethod.POST,
                 request, String.class);
+        System.out.println("디버그 : " + response.getBody());
 
         // then
-        // System.out.println(response.getStatusCode());
-        // System.out.println(response.getBody());
-
         DocumentContext dc = JsonPath.parse(response.getBody());
-        // System.out.println(dc.jsonString());
         Integer code = dc.read("$.code");
+        String username = dc.read("$.data.username");
         Assertions.assertThat(code).isEqualTo(1);
+        Assertions.assertThat(username).isEqualTo("very");
+
     }
 
 }
